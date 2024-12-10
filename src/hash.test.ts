e56d6d2452f1b3a6099e2f3301da9e0cd70157e9
Bereket Engida <bekacru@gmail.com>
2024-12-10 22:17:42 +0300

    chore: update tests

--- src/hash.test.ts
+++ src/hash.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { createHash } from "./digest";
+import { createHash } from "./hash";
 
 describe("digest", () => {
 	const inputString = "Hello, World!";
