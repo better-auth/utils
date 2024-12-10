52140594923e956a9b8f537fd5e2e78d6d8f499f
Bereket Engida <bekacru@gmail.com>
2024-12-10 19:29:16 +0300

    chore: improve hex test

--- src/hex.test.ts
+++ src/hex.test.ts
@@ -5,34 +5,39 @@ describe('hex', () => {
     describe('encode', () => {
         it('should encode a string to hexadecimal', () => {
             const input = "Hello, World!";
-            const expected = "48656c6c6f2c20576f726c6421";
-            expect(hex.encode(input)).toBe(expected);
+            expect(hex.encode(input)).toBe(
+                Buffer.from(input).toString("hex"),
+            );
         });
 
         it('should encode an ArrayBuffer to hexadecimal', () => {
             const input = new TextEncoder().encode("Hello").buffer;
-            const expected = "48656c6c6f";
-            expect(hex.encode(input)).toBe(expected);
+            expect(hex.encode(input)).toBe(
+                Buffer.from(input).toString("hex"),
+            );
         });
 
         it('should encode a TypedArray to hexadecimal', () => {
             const input = new Uint8Array([72, 101, 108, 108, 111]);
-            const expected = "48656c6c6f";
-            expect(hex.encode(input)).toBe(expected);
+            expect(hex.encode(input)).toBe(
+                Buffer.from(input).toString("hex"),
+            );
         });
     });
 
     describe('decode', () => {
         it('should decode a hexadecimal string to its original value', () => {
-            const input = "48656c6c6f2c20576f726c6421";
             const expected = "Hello, World!";
-            expect(hex.decode(input)).toBe(expected);
+            expect(hex.decode(
+                Buffer.from(expected).toString("hex"),
+            )).toBe(expected);
         });
 
         it('should handle decoding of a hexadecimal string to binary data', () => {
-            const input = "48656c6c6f";
             const expected = "Hello";
-            expect(hex.decode(input)).toBe(expected);
+            expect(hex.decode(
+                Buffer.from(expected).toString("hex"),
+            )).toBe(expected);
         });
 
         it('should throw an error for an odd-length string', () => {
@@ -62,3 +67,4 @@ describe('hex', () => {
         });
     });
 });
+
