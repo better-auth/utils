ca92e162ba2072c4ff0f7d7845e4e4f1c176710e
frectonz <fraol0912@gmail.com>
2025-08-19 02:10:58 +0300

    fix: use `BigInt`

--- src/equal.ts
+++ src/equal.ts
@@ -3,13 +3,13 @@ export function safeEqual(a: string, b: string): boolean {
 	const bBytes = new TextEncoder().encode(b);
 
 	let len = Math.max(aBytes.length, bBytes.length);
-	let result = aBytes.length ^ bBytes.length;
+	let result = BigInt(aBytes.length ^ bBytes.length);
 
 	for (let i = 0; i < len; i++) {
-		const aByte = i < aBytes.length ? aBytes[i] : 0;
-		const bByte = i < bBytes.length ? bBytes[i] : 0;
+		const aByte = i < aBytes.length ? BigInt(aBytes[i]) : 0n;
+		const bByte = i < bBytes.length ? BigInt(bBytes[i]) : 0n;
 		result |= aByte ^ bByte;
 	}
 
-	return result === 0;
+	return result === 0n;
 }
